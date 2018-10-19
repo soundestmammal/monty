e90df73fbbd1a4285f03c5ca3a67273ac8336198
Robert Checco <robert.checco@gmail.com>
2018-10-18 22:31:23 -0400

    Decision Tree Regression

--- mlaz/Part 2 - Regression/Section 4 - Simple Linear Regression/lin_reg.py
+++ mlaz/Part 2 - Regression/Section 4 - Simple Linear Regression/lin_reg.py
@@ -1,8 +1,35 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Thu Oct 18 19:12:48 2018
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
 
-@author: robertchecco
-"""
+dataset = pd.read_csv('Salary_Data.csv')
+X = dataset.iloc[:, :-1].values
+y = dataset.iloc[:, 1].values
 
+from sklearn.cross_validation import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 1/3, random_state = 0)
+
+# Fitting Simple Linear Regression to the Training Set
+
+from sklearn.linear_model import LinearRegression
+regressor = LinearRegression()
+regressor.fit(X_train, y_train)
+
+# Predicting the Test set results
+y_pred = regressor.predict(X_test)
+
+# Visualizing the Training Set Results
+
+plt.scatter(X_train, y_train, color='red')
+plt.plot(X_train, regressor.predict(X_train), color = 'blue')
+plt.title('Salary vs Experience (Training Set)')
+plt.xlabel('Years of experience')
+plt.ylabel('Salary')
+plt.show()
+
+plt.scatter(X_test, y_test, color='red')
+plt.plot(X_train, regressor.predict(X_train), color = 'blue')
+plt.title('Salary vs Experience (Test Set)')
+plt.xlabel('Years of experience')
+plt.ylabel('Salary')
+plt.show()
---
+++ mlaz/decision_tree_regression.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Oct 18 20:29:06 2018
+
+@author: robertchecco
+"""
+
