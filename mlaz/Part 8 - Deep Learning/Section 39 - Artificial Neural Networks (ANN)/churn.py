0e5f51e9fac444a05a0fdd694fc883800b35853f
Robert Checco <robert.checco@gmail.com>
2018-10-30 19:34:59 -0400

    Implement ANN on Churn Dataset

--- mlaz/Part 8 - Deep Learning/Section 39 - Artificial Neural Networks (ANN)/churn.py
+++ mlaz/Part 8 - Deep Learning/Section 39 - Artificial Neural Networks (ANN)/churn.py
@@ -17,3 +17,35 @@ onehotencoder = OneHotEncoder(categorical_features = [1])
 X = onehotencoder.fit_transform(X).toarray()
 X = X[:, 1:]
 
+# Splitting the dataset into the Training set and Test set
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
+
+# Feature Scaling
+from sklearn.preprocessing import StandardScaler
+sc = StandardScaler()
+X_train = sc.fit_transform(X_train)
+X_test = sc.transform(X_test)
+
+import keras
+from keras.models import Sequential
+from keras.layers import Dense
+
+classifier = Sequential()
+classifier.add(Dense(output_dim = 6, init='uniform', activation='relu', input_dim = 11))
+
+# Add the second hidden layer
+classifier.add(Dense(output_dim = 6, init='uniform', activation='relu'))
+
+# Add the output layer
+classifier.add(Dense(output_dim = 1, init='uniform', activation='sigmoid'))
+
+classifier.compile(optimizer='adam', loss='binary_crossentropy', metrics = ['accuracy'])
+
+# Add epochs
+classifier.fit(X_train, y_train, batch_size = 10, nb_epoch = 100)
+
+y_pred = classifier.predict(X_test)
+y_pred = (y_pred > 0.5)
+from sklearn.metrics import confusion_matrix
+cm = confusion_matrix(y_test, y_pred)
