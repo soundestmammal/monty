df293b155a5f3e4c337c5c7707b2079ed81726ab
Robert Checco <robert.checco@gmail.com>
2018-11-02 21:53:44 -0400

    More CNN practice

---
+++ mlaz/Part 8 - Deep Learning/Section 40 - Convolutional Neural Networks (CNN)/convolu.py
@@ -0,0 +1,62 @@
+
+# Part 0 - Building the Convolutional Neural Network
+
+
+from keras.models import Sequential
+from keras.layers import Convolution2D
+from keras.layers import MaxPooling2D
+from keras.layers import Flatten
+from keras.layers import Dense
+
+# Initialize the CNN
+
+classifier = Sequential()
+
+#Part 1 - Convolution
+
+# We create many feature maps to obtain
+# Our first convolutional layer
+
+classifier.add(Convolution2D(32, 3, 3, input_shape = (64, 64, 3), activation = 'relu'))
+
+# Part 2 - Max Pooling
+
+# We want to reduce the size of feature maps
+# Without losing information
+
+classifier.add(MaxPooling2D(pool_size = (2, 2)))
+
+# Part 3 Flattening (Putting numbers in single vector)
+
+classifier.add(Flatten())
+
+#Part 4 Create an ANN and use imput vector as input layer. Best for non linear problems (image recognition)
+classifier.add(Dense(output_dim = 128, activation = 'relu'))
+classifier.add(Dense(output_dim = 1, activation = 'sigmoid'))
+
+classifier.complile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['accuracy'])
+
+from keras.preprocessing.image import ImageDataGenerator
+
+train_datagen = ImageDataGenerator(rescale = 1./255,
+                                   shear_range = 0.2,
+                                   zoom_range = 0.2,
+                                   horizontal_flip = True)
+
+test_datagen = ImageDataGenerator(rescale = 1./255)
+
+training_set = train_datagen.flow_from_directory('dataset/training_set',
+                                                 target_size = (64, 64),
+                                                 batch_size = 32,
+                                                 class_mode = 'binary')
+
+test_set = test_datagen.flow_from_directory('dataset/test_set',
+                                            target_size = (64, 64),
+                                            batch_size = 32,
+                                            class_mode = 'binary')
+
+classifier.fit_generator(training_set,
+                         samples_per_epoch = 8000,
+                         nb_epoch = 25,
+                         validation_data = test_set,
+                         nb_val_samples = 2000)
