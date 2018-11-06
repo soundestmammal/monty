a30437c900f518da56173b91e24cf00250d20f71
Robert Checco <robert.checco@gmail.com>
2018-11-06 01:15:44 -0500

    Implemented Feature Scaling on the training set

--- mlaz/Part 8 - Deep Learning/RNN/recurrent.py
+++ mlaz/Part 8 - Deep Learning/RNN/recurrent.py
@@ -6,9 +6,14 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
+# Importing the Dataset
 dataset_train = pd.read_csv('Google_Stock_Price_Train.csv');
 training_set = dataset_train.iloc[:, 1:2].values
 
+# Feature Scaling
+from sklearn.preprocessing import MinMaxScaler
+sc = MinMaxScaler(feature_range = (0, 1))
+training_set_scaled = sc.fit_transform(training_set)
 
 
 # Part 2 Building the RNN
