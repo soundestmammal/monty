87b5abecf6abbbac1416da395355b23ba26b1c0c
Robert Checco <robert.checco@gmail.com>
2018-11-16 18:19:21 -0500

    V2-7 of Dataset

--- eco490w/ResearchPaper/regression.py
+++ eco490w/ResearchPaper/regression.py
@@ -9,15 +9,15 @@ dataset = dataset.drop(['TLFC1'], axis=1)
 
 clean_dataset = dataset.drop([ 'TOUR_PURP','DMPO','OMPO','ONYC','HMPO','HNYC','HSTATE','HCITY_MCD','HCOUNTY' ,'WHT_FAC3_VOCC','WHT_FAC3','TOURFAC','HH_WHT2', 'HH1','TRIPDIST','ACTDUR','SERVC','HHMEM','DTYPE', 'TOUR_ID', 'SUBT_ID', 'PERTYPE', 'PER1','GTYPE', 'HTAZ', 'HTRACT', 'OTAZ', 'DTAZ', 'NO_TAZ', 'PLOC', 'PRKTY', 'PAYPK', 'PKAMT', 'PKUNT', 'TOLFT', 'TLONB', 'TLFC1', 'TOPN1', 'TOLE1', 'TOLX1', 'TLLC1', 'TLFR1', 'TLFC2', 'TOPN2', 'TOLE2', 'TOLX2', 'TLLC2', 'TLFR2', 'TLFC3', 'TOPN3', 'TOLE3', 'TOLX3', 'TLLC3', 'TLFR3', 'O_TLFR3', 'ROUTE', 'FARE', 'FAREC', 'BUSPS', 'MTABP', 'BPFAR', 'FRBAS', 'TRP_DEP_HR', 'TRP_DEP_MIN', 'TRP_ARR_HR', 'TRP_ARR_MIN', 'OTRACT', 'DTRACT', 'OMCD', 'DMCD', 'UNIQUEID2', 'UNIQUEID3', 'UNIQUEID4', 'HHSIZ_R', 'TOD_PEAK', 'ODTPURP2_R', 'ODTPURP2', 'ODTPURP1', 'ODTPURP', 'DTPURP_R', 'DCOUNTY', 'OCOUNTY', 'ADJ_COUNTY', 'GEO_GROUP3_D', 'GEO_GROUP2_D', 'GEO_GROUP1_D', 'GEO_GROUP1_O', 'GEO_GROUP3', 'GEO_GROUP2', 'LTRIPNO', 'TOUR_ID', 'SUBT_ID', 'DTYPE', 'DOW', 'PER2', 'PER3', 'PER4', 'PER5', 'NONHH', 'VEHNO', 'DYGOV', 'PLOC' ], axis=1)
 clean_dataset.to_csv('Data_V2-4.csv', sep=',')
-new_set = pd.read_csv('Data_V2-4.csv')
+new_set = pd.read_csv('Data_V2-6.csv')
 
 new_set = new_set[new_set.GEO_GROUP1 == 3]
 new_set = new_set[new_set.TOUR_PURP == 1]
 new_set = new_set[new_set.GEO_GROUP2_O == 2]
+new_set = new_set.drop(['PMODE1', 'PAMODE', 'PMODE_R', 'PMODE_R2', 'PMODE_R3', 'WORKTRIP'], axis=1)
 
-new_set.to_csv('Data_V2-6.csv', sep=',')
+new_set.to_csv('Data_V2-7.csv', sep=',')
 
-new_set.describe()
 
 # Explanatory Variables that I am going to keep
 
@@ -33,6 +33,14 @@ new_set.describe()
 # DATA V2-6 Remove
 # new_set = new_set.drop(['OTPURP', 'OTPURP_AGG', 'DTPURP_AGG', 'MODE_SAMP', 'TRPDUR_HN', 'DZIP', 'HZIP', 'WORK_PURP' ], axis=1)
 
+# Data V2-7 REmove
+new_set = new_set.drop(['PMODE1', 'PAMODE', 'PMODE_R', 'PMODE_R2', 'PMODE_R3', 'WORKTRIP'], axis=1)
+new_set = new_set.drop(['GEO_GROUP2_O', 'GEO_GROUP3_O'], axis=1)
+new_set = new_set.drop(['TRIPDIST_R1', 'TRIPDIST_R2', 'TRPDUR_R', 'TOD_R1', 'TOD_R'], axis=1)
+
+
+
+
 
 # AfterI drop all of the unwanted data
 
@@ -41,4 +49,5 @@ new_set.describe()
 # 2. I want to create a seperate set for just Long Island Reisdents
 
 # 3. I want to see if the sample size ia big enough
+
 
